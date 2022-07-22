82cdf6662f92f3799b58cbe3c0ea7a535012c81e
Ned Schwartz <ned@theinterned.net>
2022-07-22 11:38:49 -0400

    show post markdown on posts/

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,10 +2,11 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
+import { marked } from "marked";
 import type { Post } from "~/models/post.server";
 import { getPost } from "~/models/post.server";
 
-type LoaderData = { post: Post };
+type LoaderData = { post: Pick<Post, "title" | "markdown">; html: string };
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, `params.slug is required`);
@@ -13,15 +14,18 @@ export const loader: LoaderFunction = async ({ params }) => {
   const post = await getPost(params.slug);
   invariant(post, `Post not fount: ${params.slug}`);
 
-  return json<LoaderData>({ post });
+  const html = marked(post.markdown);
+
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData() as LoaderData;
+  const { post, html } = useLoaderData() as LoaderData;
 
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
