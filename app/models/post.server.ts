a596f76dde7ac606cff58cac16d1319aed88de0d
Ned Schwartz <ned@theinterned.net>
2022-07-22 10:59:31 -0400

    add Post model and seed to prisma

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,11 +1,10 @@
+import { prisma } from "~/db.server";
+
 type Post = {
   slug: string;
   title: string;
 };
 
 export async function getPosts(): Promise<Post[]> {
-  return [
-    { slug: "my-first-post", title: "My first post" },
-    { slug: "90s-mixtape", title: "A Mixtape I made just for you" },
-  ];
+  return prisma.post.findMany();
 }
